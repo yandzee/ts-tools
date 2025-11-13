7345e257e104a6e15c0c9920c45e1976a4b2390b
Renat Tuktarov <yandzeek@gmail.com>
2025-11-14 01:59:43 +0400

    use tsdown to build and watch loader package

---
+++ packages/tsdown/tsdown.base.ts
@@ -0,0 +1,9 @@
+import { defineConfig as _defineConfig } from 'tsdown';
+import type { UserConfig } from 'tsdown';
+
+export const defineConfig = (cfg: UserConfig) => {
+	return _defineConfig([
+		{ ...cfg, platform: 'browser' },
+		{ ...cfg, platform: 'node' },
+	]);
+};
